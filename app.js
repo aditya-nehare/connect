d5b8c8ae799d2c8cd3d62480c5f6b71f894283cf
aditya-nehare <adityanehare571@gmail.com>
2025-06-29 12:33:15 +0530

    ....

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const Listing = require("./models/listing");
 
@@ -29,6 +30,9 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public"))); //for satic file
+
+app.engine("ejs", ejsMate);
 
 // Root route
 app.get("/", (req, res) => {
